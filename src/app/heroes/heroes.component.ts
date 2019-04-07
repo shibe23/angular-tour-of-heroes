9395303e639cd806cc5011d2f9223fa0dea47b98
shibe23 <shibe23.info@gmail.com>
2019-04-07 18:04:01 +0900

    Add Heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  constructor() {}
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-  constructor() { }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
